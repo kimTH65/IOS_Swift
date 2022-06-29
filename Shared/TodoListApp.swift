a6a3489d58f1f09ae971afab1e8ae6ee9ceec58d
kimtaehoon <xogns6515@gmail.com>
2022-06-29 14:29:58 +0900

    start_ios

---
+++ Shared/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  Shared
+//
+//  Created by 金 泰勳 on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
