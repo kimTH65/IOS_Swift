352d5f9820300921374f1acaab74b34e4291599b
金 泰勳 <kimtaehoon@ais-info.co.jp>
2022-08-03 16:10:27 +0900

    no message

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todoList
+//
+//  Created by 金 泰勳 on 2022/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
